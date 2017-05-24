d1f90cd51aa963da89eeb1c531b0da051cffa676
Marc Honnorat <marc.honnorat@exwexs.fr>
2017-05-24 08:47:41 +0200

    Clean repo and add .gitignore

--- config.h
+++ config.h
@@ -1,82 +1,86 @@
+/* config.h.  Generated from config.h.in by configure.  */
 /* config.h.in.  Generated from configure.in by autoheader.  */
 
 /* Define to 1 if you have the <inttypes.h> header file. */
-#undef HAVE_INTTYPES_H
+#define HAVE_INTTYPES_H 1
 
 /* Define to 1 if you have the `expat' library (-lexpat). */
-#undef HAVE_LIBEXPAT
+#define HAVE_LIBEXPAT 1
 
 /* Define to 1 if you have the `X11' library (-lX11). */
-#undef HAVE_LIBX11
+#define HAVE_LIBX11 1
 
 /* Define to 1 if you have the `Xaw' library (-lXaw). */
-#undef HAVE_LIBXAW
+#define HAVE_LIBXAW 1
 
 /* Define to 1 if you have the `Xaw3d' library (-lXaw3d). */
-#undef HAVE_LIBXAW3D
+/* #undef HAVE_LIBXAW3D */
 
 /* Define to 1 if you have the `Xt' library (-lXt). */
-#undef HAVE_LIBXT
+#define HAVE_LIBXT 1
 
 /* Define to 1 if you have the <memory.h> header file. */
-#undef HAVE_MEMORY_H
+#define HAVE_MEMORY_H 1
 
 /* Define if you have the NETCDF library, either v3 or v4 */
-#undef HAVE_NETCDF
+#define HAVE_NETCDF 1
 
 /* Define if you have version 4 of the NETCDF library */
-#undef HAVE_NETCDF4
+#define HAVE_NETCDF4 1
 
 /* Define if you have PNG library */
-#undef HAVE_PNG
+#define HAVE_PNG 1
 
 /* Define to 1 if you have the <stdint.h> header file. */
-#undef HAVE_STDINT_H
+#define HAVE_STDINT_H 1
 
 /* Define to 1 if you have the <stdlib.h> header file. */
-#undef HAVE_STDLIB_H
+#define HAVE_STDLIB_H 1
 
 /* Define to 1 if you have the <strings.h> header file. */
-#undef HAVE_STRINGS_H
+#define HAVE_STRINGS_H 1
 
 /* Define to 1 if you have the <string.h> header file. */
-#undef HAVE_STRING_H
+#define HAVE_STRING_H 1
 
 /* Define to 1 if you have the <sys/stat.h> header file. */
-#undef HAVE_SYS_STAT_H
+#define HAVE_SYS_STAT_H 1
 
 /* Define to 1 if you have the <sys/types.h> header file. */
-#undef HAVE_SYS_TYPES_H
+#define HAVE_SYS_TYPES_H 1
 
 /* Define if you have udunits-2 library */
-#undef HAVE_UDUNITS2
+#define HAVE_UDUNITS2 1
 
 /* Define to 1 if you have the <unistd.h> header file. */
-#undef HAVE_UNISTD_H
+#define HAVE_UNISTD_H 1
 
 /* Name of package */
-#undef PACKAGE
+#define PACKAGE "ncview"
 
 /* Define to the address where bug reports for this package should be sent. */
-#undef PACKAGE_BUGREPORT
+#define PACKAGE_BUGREPORT "dpierce@ucsd.edu"
 
 /* Define to the full name of this package. */
-#undef PACKAGE_NAME
+#define PACKAGE_NAME "ncview"
 
 /* Define to the full name and version of this package. */
-#undef PACKAGE_STRING
+#define PACKAGE_STRING "ncview 2.0.0"
 
 /* Define to the one symbol short name of this package. */
-#undef PACKAGE_TARNAME
+#define PACKAGE_TARNAME "ncview"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#undef PACKAGE_VERSION
+#define PACKAGE_VERSION "2.0.0"
 
 /* Define to 1 if you have the ANSI C header files. */
-#undef STDC_HEADERS
+#define STDC_HEADERS 1
 
 /* Version number of package */
-#undef VERSION
+#define VERSION "2.0.0"
 
 /* Define to 1 if the X Window System is missing or not being used. */
-#undef X_DISPLAY_MISSING
+/* #undef X_DISPLAY_MISSING */
